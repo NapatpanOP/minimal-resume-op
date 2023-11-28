782561e8a4278f59c6ffa4ff0f4bf7f863e2aca0
NapatpanOP <napatpanns424@gmail.com>
2023-11-28 15:56:26 +0700

    Add tailwind variable and image profile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'white-1' 	: '#FFFCF2',
+				'gray-1' 		: '#403D39',
+				'gray-2' 		: '#5F5B57',
+				'gray-3' 		: '#A8A4A0',
+			},
+		},
 	},
 	plugins: [],
 }
